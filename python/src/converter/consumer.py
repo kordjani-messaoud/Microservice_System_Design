644885c8e2696d7e08b8129591b3f5c571fbc068
KORDJANI Messaoud Nadjib <kordjanimessaoudnadjib@gmail.com>
2024-11-02 12:39:59 +0100

    Fix converter service consumer
    Add missing instructions to converter's consumer at line 40 and 41

--- python/src/converter/consumer.py
+++ python/src/converter/consumer.py
@@ -37,6 +37,9 @@ def main():
         on_message_callback=callback
         )
 
+    print("Waiting for messages. To exit press CTRL+C")
+    channel.start_consuming()
+
 if __name__ == '__main__':
     try:
         main()
