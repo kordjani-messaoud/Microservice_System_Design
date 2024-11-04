e03faa759169d91eb95e722b060273e4733f1a3b
KORDJANI Messaoud Nadjib <kordjanimessaoudnadjib@gmail.com>
2024-11-04 22:42:31 +0100

    FIX: Fix auth service's bugs
    Correct container image version and service name in the deployments
    Fix MySQL server port number type issue
    Remove problematic res variable and add sanitization to the SQL request in server.py
    Install correct JWT package

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -24,41 +24,50 @@ server.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST')
 server.config['MYSQL_USER'] = os.environ.get('MYSQL_USER')
 server.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD')
 server.config['MYSQL_DB'] = os.environ.get('MYSQL_DB')
-server.config['MYSQL_PORT'] = os.environ.get('MYSQL_PORT')
+server.config['MYSQL_PORT'] = int(os.environ.get('MYSQL_PORT'))
 
 @server.route('/login', methods=['POST'])
 def login():
+    print('Auth login route')
     auth = request.authorization
+    print(f'Authorization heade of the request{request.authorization}')
     if not auth:
+
         return 'Unauthorized, Missing Credentials', 401
+
     cur = mysql.connection.cursor()
-    res = cur.execute(
-        f'SELECT name, email, password FROM user WHERE email {auth.username}'
-    )
-    if len(res) > 0:
+    
+    try:
+        cur.execute(
+            'SELECT email, password FROM user WHERE email = %s', (auth.username,)
+        )
+    except:
+        return 'Error SQL request', 555
+    else:
         user_row = cur.fetchone()
         email = user_row[0]
-        password = user_row[0]
+        password = user_row[1]
         if auth.username != email or auth.password != password:
             return 'Unauthorized, Invalid Credentials', 401
         else:
-            return create_JWT(auth.username, os.environ.get('JWT_SECRET'), True)
-    else:
-        return 'Invalide credentials', 401
+            return create_JWT(auth.username, os.environ.get("JWT_SECRET"), True)
 
 @server.route('/validate', methods=['POST'])
 def validate():
-    encoded_jwt = request.headers('Authorization')
+    encoded_jwt = request.headers['Authorization']
     if not encoded_jwt:
         return 'Unauthorized, Missing Credentials', 401
     encoded_jwt = encoded_jwt.split(' ')[1]
 
     try:
-        decoded_token = encoded_jwt.decode(encoded_jwt, os.environ.get('JWT_SECRET'),
-                                           algorithms=['HS256']
-                                           )
+        decoded_token = jwt.decode(
+            encoded_jwt,
+            os.environ.get('JWT_SECRET'),
+            algorithms=['HS256']
+            )
     except:
         return 'Forbidden', 403
+        
     return decoded_token, 200
 
 if __name__ == '__main__':
