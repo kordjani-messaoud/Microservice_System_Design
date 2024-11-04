88e70d9db173752ce282af8bd7e88268d8a67288
KORDJANI Messaoud Nadjib <kordjanimessaoudnadjib@gmail.com>
2024-11-04 21:51:44 +0100

    Fix: Change auth_user localtion
    Change auth_user location to allow   access to MySQL DB from Minikube node

--- python/src/auth/init.sql
+++ python/src/auth/init.sql
@@ -1,16 +1,17 @@
-CREATE USER 'auth_user'@'localhost' IDENTIFIED BY 'auth_password';
+-- CREATE USER 'auth_user'@'localhost' IDENTIFIED BY 'auth_password';
+CREATE USER 'auth_user'@'192.168.59.100' IDENTIFIED BY 'auth_password';
 
 CREATE DATABASE auth;
 
-GRANT ALL PRIVILEGES ON auth.* TO 'auth_user'@'localhost';
+-- GRANT ALL PRIVILEGES ON auth.* TO 'auth_user'@'localhost';
+GRANT ALL PRIVILEGES ON auth.* TO 'auth_user'@'192.168.50.100';
 
 USE auth;
 
 CREATE TABLE user (   
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
 
-INSERT INTO user (name, email, password) VALUES ('messaoud', 'messaoud@email.com', 'messaoud');
+INSERT INTO user (email, password) VALUES ('messaoud', 'messaoud');
